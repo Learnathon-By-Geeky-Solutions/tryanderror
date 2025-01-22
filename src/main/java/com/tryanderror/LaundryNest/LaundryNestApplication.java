90c2ae007094cbae85274c62d7b904c7d254da14
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2025-01-22 23:30:11 +0600

    Startup files

---
+++ src/main/java/com/tryanderror/LaundryNest/LaundryNestApplication.java
@@ -0,0 +1,13 @@
+package com.tryanderror.LaundryNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tryanderror/LaundryNest/LaundryNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryanderror.LaundryNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaundryNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
